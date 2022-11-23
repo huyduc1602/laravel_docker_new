98b201f6d0bce0f15802169dc6255b6fbd9c073b
Le Tran Minh Nhut <nhutltm@ids-v.local>
2022-11-23 14:32:59 +0700

    delete

--- project/resources/views/layout/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-<footer>
-    Copyright@Nippon Computer Dynamic Co., Ltd. All Rights Reserved
-</footer>
--- vendor/blade-ui-kit/blade-ui-kit/resources/views/components/buttons/form-button.blade.php
+++
@@ -1,8 +0,0 @@
-<form method="POST" @isset($action) action="{{ $action }}" @endisset>
-    @csrf
-    @method($method)
-
-    <button type="submit" {{ $attributes }}>
-        {{ $slot }}
-    </button>
-</form>
